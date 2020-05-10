03fd62bb44718c4e2fe5c37a8bef2ac8207b4b8a
Ken Moini <ken@kenmoini.com>
2020-05-10 11:34:02 -0500

    Update sat6-on-rhv4.3.sh

--- sat6-on-rhv4.3.sh
+++ sat6-on-rhv4.3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Subscribe...
+
+subscription-manager repos --disable "*"
 subscription-manager repos --enable=rhel-7-server-rpms --enable=rhel-7-server-satellite-6.7-rpms --enable=rhel-7-server-satellite-maintenance-6-rpms --enable=rhel-server-rhscl-7-rpms --enable=rhel-7-server-ansible-2.8-rpms --enable=rhel-7-server-rh-common-rpms
+
+grubby --update-kernel=ALL --args="spectre_v2=off nopti"
 
 yum clean all
 
@@ -10,11 +15,16 @@ yum update -y && systemctl reboot
 
 yum install satellite nano ovirt-guest-agent-common
 
+firewall-cmd --add-port="80/tcp" --add-port="443/tcp" --add-port="5647/tcp" --add-port="8000/tcp" --add-port="8140/tcp" --add-port="9090/tcp" --add-port="53/udp" --add-port="53/tcp" --add-port="67/udp" --add-port="69/udp" --add-port="5000/tcp"
+firewall-cmd --runtime-to-permanent
+
 systemctl start ovirt-guest-agent
 systemctl enable ovirt-guest-agent
 
 systemctl start qemu-guest-agent
 systemctl enable qemu-guest-agent
+
+yum update -y && systemctl reboot
 
 satellite-installer --scenario satellite \
 --foreman-initial-admin-username admin \
